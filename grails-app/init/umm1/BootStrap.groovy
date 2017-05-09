554fa5f95c2d7402e1321a21de1931416d066ece
saqibahmed515 <saqibahmed515@gmail.com>
2017-05-09 18:53:02 +0500

    set up bootstrap for deployment.

--- grails-app/init/umm1/BootStrap.groovy
+++ grails-app/init/umm1/BootStrap.groovy
@@ -7,75 +7,70 @@ import groovy.json.JsonBuilder
 class BootStrap {
 
     def init = { servletContext ->
-        if (Role.count() == 0) {
-            try {
+        if(Permission.count() == 0){
+            try{
                 //permissions related to user controller
-                new Permission(name: "com.ef.umm.user.show", expression: "user:show:*")
-                new Permission(name: "com.ef.umm.user.update", expression: "user:update:*")
-                new Permission(name: "com.ef.umm.user.create", expression: "user:create:*")
-                new Permission(name: "com.ef.umm.user.delete", expression: "user:delete:*")
-                new Permission(name: "com.ef.umm.user.updatePassword", expression: "user:updatePassword:*")
-                new Permission(name: "com.ef.umm.user.resetPassword", expression: "user:resetPassword:*")
-                new Permission(name: "com.ef.umm.user.list", expression: "user:list:*")
-                new Permission(name: "com.ef.umm.user.addRevokeMicroserviceRoles", expression: "user:addRevokeMicroserviceRoles:*")
+                new Permission(name: "com.ef.umm.user.show", expression: "user:show:*").save()
+                new Permission(name: "com.ef.umm.user.update", expression: "user:update:*").save()
+                new Permission(name: "com.ef.umm.user.create", expression: "user:create:*").save()
+                new Permission(name: "com.ef.umm.user.delete", expression: "user:delete:*").save()
+                new Permission(name: "com.ef.umm.user.updatePassword", expression: "user:updatePassword:*").save()
+                new Permission(name: "com.ef.umm.user.resetPassword", expression: "user:resetPassword:*").save()
+                new Permission(name: "com.ef.umm.user.list", expression: "user:list:*").save()
+                new Permission(name: "com.ef.umm.user.addRevokeMicroserviceRoles", expression: "user:addRevokeMicroserviceRoles:*").save()
 
                 //permissions related to microservice controller
-                new Permission(name: "com.ef.umm.microservice.show", expression: "microservice:show:*")
-                new Permission(name: "com.ef.umm.microservice.update", expression: "microservice:update:*")
-                new Permission(name: "com.ef.umm.microservice.create", expression: "microservice:create:*")
-                new Permission(name: "com.ef.umm.microservice.delete", expression: "microservice:delete:*")
-                new Permission(name: "com.ef.umm.microservice.list", expression: "microservice:list:*")
-                new Permission(name: "com.ef.umm.microservice.addRemoveRoles", expression: "microservice:addRemoveRoles:*")
+                new Permission(name: "com.ef.umm.microservice.show", expression: "microservice:show:*").save()
+                new Permission(name: "com.ef.umm.microservice.update", expression: "microservice:update:*").save()
+                new Permission(name: "com.ef.umm.microservice.create", expression: "microservice:create:*").save()
+                new Permission(name: "com.ef.umm.microservice.delete", expression: "microservice:delete:*").save()
+                new Permission(name: "com.ef.umm.microservice.list", expression: "microservice:list:*").save()
+                new Permission(name: "com.ef.umm.microservice.addRemoveRoles", expression: "microservice:addRemoveRoles:*").save()
 
                 //permissions related to role controller
-                new Permission(name: "com.ef.umm.role.show", expression: "role:show:*")
-                new Permission(name: "com.ef.umm.role.update", expression: "role:update:*")
-                new Permission(name: "com.ef.umm.role.create", expression: "role:create:*")
-                new Permission(name: "com.ef.umm.role.delete", expression: "role:delete:*")
-                new Permission(name: "com.ef.umm.role.list", expression: "role:list:*")
-                new Permission(name: "com.ef.umm.role.addRevokePermissions", expression: "role:addRevokePermissions:*")
+                new Permission(name: "com.ef.umm.role.show", expression: "role:show:*").save()
+                new Permission(name: "com.ef.umm.role.update", expression: "role:update:*").save()
+                new Permission(name: "com.ef.umm.role.create", expression: "role:create:*").save()
+                new Permission(name: "com.ef.umm.role.delete", expression: "role:delete:*").save()
+                new Permission(name: "com.ef.umm.role.list", expression: "role:list:*").save()
+                new Permission(name: "com.ef.umm.role.addRevokePermissions", expression: "role:addRevokePermissions:*").save()
 
                 //permissions related to permission controller
-                new Permission(name: "com.ef.umm.permission.show", expression: "permission:show:*")
-                new Permission(name: "com.ef.umm.permission.update", expression: "permission:update:*")
-                new Permission(name: "com.ef.umm.permission.create", expression: "permission:create:*")
-                new Permission(name: "com.ef.umm.permission.delete", expression: "permission:delete:*")
-                new Permission(name: "com.ef.umm.permission.list", expression: "permission:list:*")
-
-                
-                def admin = new User(username: "admin", password: "admin")
-                def hamid = new User(username: "hamid", password: "nothing")
-                admin?.save(flush: true, failOnError: true)
-                hamid?.save(flush: true, failOnError: true)
-
+                new Permission(name: "com.ef.umm.permission.show", expression: "permission:show:*").save()
+                new Permission(name: "com.ef.umm.permission.update", expression: "permission:update:*").save()
+                new Permission(name: "com.ef.umm.permission.create", expression: "permission:create:*").save()
+                new Permission(name: "com.ef.umm.permission.delete", expression: "permission:delete:*").save()
+                new Permission(name: "com.ef.umm.permission.list", expression: "permission:list:*").save()
+            }catch (Exception ex){
+                log.error("Exception occured in bootstrap while creating permissions: ", ex)
+                println ex.getMessage()
+            }
+        }
+        if (Role.count() == 0) {
+            try {
+                def admin
+                def umm
                 def roleAdmin = new Role(authority: "ROLE_ADMIN", description: "Administrator Role")
                 roleAdmin.addToPermissions(name: "com.ef.umm.admin", expression: "*:*")
                 roleAdmin?.save(flush: true, failOnError: true)
 
                 def role = new Role(authority: "ROLE_USER", description: "User role")
-                role.addToPermissions(name: "com.ef.umm.user.show", expression: "user:show:*")
-                role.addToPermissions(name: "com.ef.umm.user.update", expression: "user:update:*")
                 role?.save(flush: true, failOnError: true)
 
-                def umm = new Microservice(name: 'UMM', description: 'User Management MicroService')
-                def pcs = new Microservice(name: 'PCS', description: 'Post Call Survey')
-                def cbr = new Microservice(name: 'CBR', description: 'Caller Based Routing')
-                pcs.addToRoles(roleAdmin)
-                pcs.addToRoles(role)
-                pcs?.save(flush: true, failOnError: true)
-                cbr.addToRoles(roleAdmin)
-                cbr.addToRoles(role)
-                cbr?.save(flush: true, failOnError: true)
-                umm.addToRoles(roleAdmin)
-                umm.addToRoles(role)
-                umm?.save(flush: true, failOnError: true)
+                if (!User.findByUsername("admin")) {
+                    admin = new User(username: "admin", password: "admiN123!")
+                    admin?.save(flush: true, failOnError: true)
+                }
 
+                if(!Microservice.findByName("UMM")){
+                    umm = new Microservice(name: 'UMM', description: 'User Management MicroService')
+                    umm.addToRoles(roleAdmin)
+                    umm.addToRoles(role)
+                    umm?.save(flush: true, failOnError: true)
+                }
 
-
-                UMR.create admin, roleAdmin, pcs
-                UMR.create admin, role, cbr
-                UMR.create hamid, roleAdmin, cbr
                 UMR.create admin, roleAdmin, umm
+                UMR.create admin, role, umm
 
                 UMR.withSession {
                     it.flush()
@@ -83,6 +78,7 @@ class BootStrap {
                 }
             }
             catch (Exception ex){
+                log.error("Exception occured in bootstrap while creating permissions: ", ex)
                 println ex.getMessage()
             }
 
